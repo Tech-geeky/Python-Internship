9906f2b45e65635bf6bc50f667d106191adaee2c
HARIKRISHNAN P <107164319+Tech-geeky@users.noreply.github.com>
2023-10-24 10:47:53 +0530

    Update Password Generator (Task 2).py

--- Password Generator (Task 2).py
+++ Password Generator (Task 2).py
@@ -8,19 +8,16 @@ def generate_password():
     password = ''.join(random.choice(chars) for i in range(length))
     result_label.config(text=f"Generated Password: {password}")
 
-
 window = tk.Tk()
-window.title("Password Generator by Sachin ")
-
+window.title("Password Generator")
 
 length_label = tk.Label(window, text="Password Length:")
 length_label.pack(pady=(10,0))
 length_entry = tk.Entry(window)
 length_entry.pack(pady=(0,10))
 
-generate_button = tk.Button(window, text="Generate Password", command=generate_password)
+generate_button = tk.Button(window, text="Generate Password", command=generate_password, fg='white', bg='orange', font=('Arial', 12), padx=10, pady=5)  # Customize text color, background color, font, and padding
 generate_button.pack(pady=(0,10))
-
 
 result_label = tk.Label(window, font=("Helvetica", 12), wraplength=300)
 result_label.pack()
