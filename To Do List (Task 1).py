5eeafaf164894bb512a6cd06be48320f031d0e13
HARIKRISHNAN P <107164319+Tech-geeky@users.noreply.github.com>
2023-10-24 10:49:00 +0530

    Update To Do List (Task 1).py

--- To Do List (Task 1).py
+++ To Do List (Task 1).py
@@ -13,26 +13,30 @@ def remove_task():
     except:
         pass
 
+def on_add_button_hover(event):
+    add_button.config(fg='white', bg='blue')  # Change text color to white and background color to blue on hover
+
+def on_add_button_leave(event):
+    add_button.config(fg='black', bg='lightgray')  # Restore text color to black and background color to light gray
+
 def main():
-    global entry, listbox
+    global entry, listbox, add_button
 
     root = tk.Tk()
     root.title("To-Do List")
 
-    
     entry = tk.Entry(root, width=50)
     entry.pack(pady=10)
 
-    
-    add_button = tk.Button(root, text="Add Task", command=add_task)
+    add_button = tk.Button(root, text="Add Task", command=add_task, fg='black', bg='lightgray', font=('Arial', 14))  # Customize text color, background color, and font
     add_button.pack(pady=5)
+    add_button.bind("<Enter>", on_add_button_hover)  # Handle hover event
+    add_button.bind("<Leave>", on_add_button_leave)  # Handle leaving hover
 
-  
-    remove_button = tk.Button(root, text="Remove Task", command=remove_task)
+    remove_button = tk.Button(root, text="Remove Task", command=remove_task, fg='white', bg='red', font=('Arial', 14))  # Customize text color, background color, and font
     remove_button.pack(pady=5)
 
-    
-    listbox = tk.Listbox(root, width=50)
+    listbox = tk.Listbox(root, width=50, font=('Arial', 12))  # Customize font for the listbox
     listbox.pack(pady=10)
 
     root.mainloop()
